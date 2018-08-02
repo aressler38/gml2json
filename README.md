97725221becce211de67530f61844f5cca9879e6
alexander <alexander.ressler@gmail.com>
2018-08-02 06:27:17 +0000

    api

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gml2json
 
 Convert gml format to json format via jison.
+
+
+# Usage
+
+
+    const gml2json = require('gml2json');
+
+    // Parse a raw gml string.
+    let result = gml2json.parse(gmlString);
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var jison = require('jison');
+const
+    jison = require('jison'),
+    path = require('path'),
+    grammar = require(path.join(
+        __dirname, 'src', 'gml_grammar.js'));
+
+module.exports = {
+    parse,
+};
+
+function parse (string) {
+    const parser = new jison.Parser(grammar);
+    return parser.parse(string);
+}
